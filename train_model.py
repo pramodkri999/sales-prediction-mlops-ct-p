558cef3a34d6653ad5e5244bababae7610a0b0f1
Pramod K <pramod.k@fractal.ai>
2026-02-10 18:09:43 +0530

    second commit

--- train_model.py
+++ train_model.py
@@ -6,6 +6,6 @@ from training_pipeline import train_and_evaluate
 if __name__ == "__main__":
     metrics = train_and_evaluate()
 
-    print("Model trained successfully, ready to push into remote repository")
+    print("Model trained successfully, ready to push into remote repository (dockerhub)")
     print(f"R2 Score: {metrics['r2']:.3f}")
     print(f"MSE: {metrics['mse']:.3f}")
